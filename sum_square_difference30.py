ef23b6dd5008f7ea6db99323c2f90a43d27b6c67
Nestor Vega <vega.nestor@yahoo.com>
2022-03-01 10:36:22 -0800

    updated this PR instead of making a new PR

--- sum_square_difference30.py
+++ sum_square_difference30.py
@@ -1,13 +1,23 @@
-tot = 0
-tots = 0
-res = 0
-for x in range(101):
-    a = (x ** 2)
-    tot = tot + a
-    print(tot)
-for x in range(1,101):
-    tots = tots + x
-    res = tots ** 2
-    print(res)
+# The sum of the squares of the first ten natural numbers is, 1² + 2² + 3²...+ 10² = 385
 
-print(res - tot)
+# The square of the sum of the first ten natural numbers is, (1+2+3...+10)²=55²= 3025
+
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+# 3025 - 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
+def First_Ten():
+    first = 0
+    tots = 0
+    res = 0
+    for x in range(101):
+        a = (x ** 2)
+        first = first + a
+        sp = (first)
+    for x in range(101):
+        tots = tots + x
+        res = (tots ** 2)
+        all = (res)
+    print(all - sp)
+if __name__ == '__main__':
+    First_Ten() 
