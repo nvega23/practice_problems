59446584580f9e8e2a40607aba54f2ce6bf5968f
Nestor Vega <vega.nestor@yahoo.com>
2022-03-02 15:15:58 -0800

    fixed spacing and comments

--- sum_square_difference30.py
+++ sum_square_difference30.py
@@ -1,11 +1,15 @@
-# The sum of the squares of the first ten natural numbers is, 1² + 2² + 3²...+ 10² = 385
+# The sum of the squares of the first ten natural numbers is,
+# 1² + 2² + 3²...+ 10² = 385
 
-# The square of the sum of the first ten natural numbers is, (1+2+3...+10)²=55²= 3025
+# The square of the sum of the first ten natural numbers is,
+# (1+2+3...+10)²=55²= 3025
 
-# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+# Hence the difference between the sum of the squares of the first ten natural
+# numbers and the square of the sum is 
 # 3025 - 385 = 2640.
 
-# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
+# Find the difference between the sum of the squares of the first one hundred
+# natural numbers and the square of the sum
 
 
 def first_ten():
@@ -13,24 +17,22 @@ def first_ten():
     tots = 0
     res = 0
 
-    # the first for loop will gather the 1² + 2² + 3²...+ 10² = 385 or wichever number is wanted 
-
+    # the first for loop will gather the 1² + 2² + 3²...+ 10² = 385 or wichever
+    # number is wanted
     for x in range(101):
         first += x ** 2
         sums = (first)
 
-
-    # this second for loop will gather the (1+2+3...+10)²=55²= 3025 or any other number and subtract from the two loops 
-
+    # this second for loop will gather the (1+2+3...+10)²=55²= 3025 or any
+    # other number and subtract from the two loops
     for x in range(101):
         tots += x
         res = (tots ** 2)
-        all = (res)
-    print(f'{all:,}')
+        result = (res - sums)
+
+    return result
 
 
 if __name__ == '__main__':
-    first_ten() 
-
-
-    
+    result = first_ten()
+    print(result)
