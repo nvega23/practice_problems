cb888503597582ef1a1aa699f3d75387ab7cc32b
Nestor Vega <vega.nestor@yahoo.com>
2022-03-01 17:02:15 -0800

    fixed function,added space between code and comments,fixed variable names and simplified

--- sum_square_difference30.py
+++ sum_square_difference30.py
@@ -6,18 +6,34 @@
 # 3025 - 385 = 2640.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
-def First_Ten():
+
+
+
+def first_ten():
     first = 0
     tots = 0
     res = 0
     for x in range(101):
-        a = (x ** 2)
-        first = first + a
-        sp = (first)
+        first += x ** 2
+        sums = (first)
+
+
+        # the first for loop will gather the 1² + 2² + 3²...+ 10² = 385 or wichever number is wanted 
+
+
     for x in range(101):
-        tots = tots + x
+        tots += x
         res = (tots ** 2)
         all = (res)
-    print(all - sp)
+    print(all - sums)
+
+
+        # this second for loop will gather the (1+2+3...+10)²=55²= 3025 or any other number and subtract from the two loops 
+
+
+
 if __name__ == '__main__':
-    First_Ten() 
+    first_ten() 
+
+
+    
