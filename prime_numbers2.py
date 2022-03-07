86a270d84b110ea97e76bd58784c28f9cf82262d
Nestor Vega <93811834+nvega23@users.noreply.github.com>
2022-03-07 15:16:41 -0800

    project euler: 5 (#4)
    * this one took me 30 min
    
    * updated this PR instead of making a new PR
    
    * fixed function,added space between code and comments,fixed variable names and simplified
    
    * finding the smallest positive number divisible by all num 1 - 20
    
    * smallest multiple
    
    * fixed spacing
    
    * tab size to 4,space after comma,included if name,fixed spacing
    
    * this one took me 30min
    
    * Delete sum_square_difference.py
    
    * fixed spacing and comments
    
    * placed a return call in main
    
    * removed unnecessary :
    
    * 6th prime = 13. so what is 10 001 prime number
    
    * fixed spacing
    
    * tab size to 4,space after comma,included if name,fixed spacing
    
    * revert file
    
    * fixed branches
    
    * spacing
    
    * renamed file
    
    * renamed file
    
    * fixed spaces
    
    * renamed file

---
+++ prime_numbers2.py
@@ -0,0 +1,32 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+# that the 6th prime is 13.
+
+# What is the 10,001st prime number?
+
+def primes():
+    div = 10001
+    i = 2
+    count = 0
+    prime = 0
+    now = []
+
+    while count < div:
+
+        for x in range (2,i + 1):
+            if len(now) == 2:
+                break
+            elif i % x == 0:
+                now.append(x)
+
+        if len(now)==1:
+            prime = i
+            count += 1
+        now = []
+        i += 1
+
+    return prime
+
+
+if __name__ == '__main__':
+    result = primes()
+    print(result)
