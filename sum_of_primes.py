b757c6e3346433744a17f1b2ab994b94e0bf3d6c
Nestor Vega <vega.nestor@yahoo.com>
2022-03-09 20:53:07 -0800

    summation of primes

---
+++ sum_of_primes.py
@@ -0,0 +1,16 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+def sum():
+    for x in range(2, 10):
+        for y in range(2, x):
+            if y % x == 0:
+                print(y)
+    result = (x + y)
+
+    return result
+
+
+if __name__ == '__main__':
+    result = sum()
+    print(result)
