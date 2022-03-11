c2b96da9102ed75b124e96e4939e6c649d4a2042
Nestor Vega <vega.nestor@yahoo.com>
2022-03-10 22:58:45 -0800

    modifying my def and placing an argument and including an assert syntax

--- sum_of_primes.py
+++ sum_of_primes.py
@@ -1,16 +1,17 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
-def sum():
-    for x in range(2, 10):
-        for y in range(2, x):
+def sum(num1, num2):
+    for x in range(num1, num2):
+        for y in range(20, x):
             if y % x == 0:
-                return y 
+                return y
     result = (x + y)
 
     return result
 
 
 if __name__ == '__main__':
-    result = sum()
+    result = sum(1999999, 2000000)
     print(result)
+    assert sum(1999999, 2000000) == 3999997
