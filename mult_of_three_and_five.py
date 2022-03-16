bcd22f20033d7d7dcd59b7f6cb3d8a0579660d45
Nestor Vega <vega.nestor@yahoo.com>
2022-03-16 12:55:34 -0700

    multiples of three and fives

--- mult_of_three_and_five.py
+++ mult_of_three_and_five.py
@@ -3,7 +3,7 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def mult(num1):
+def mults_three_five(num1):
     total = 0
     for x in range(num1):
         if (x % 3 == 0 or x % 5 == 0):
@@ -13,11 +13,9 @@ def mult(num1):
 
 
 if __name__ == "__main__":
-    total = mult(1000)
-    print(total)
-    assert mult(1000) == 233168
-    assert mult(1020) == 242250
-    assert mult(1300) == 394118
-    assert mult(1005) == 235170
-    assert mult(10) == 23
+    assert mults_three_five(1000) == 233168
+    assert mults_three_five(1020) == 242250
+    assert mults_three_five(1300) == 394118
+    assert mults_three_five(1005) == 235170
+    assert mults_three_five(10) == 23
     print("test passed!")
