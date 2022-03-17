7475254af19740adc16c0dedf29e9c79ab5119a3
Nestor Vega <vega.nestor@yahoo.com>
2022-03-16 20:16:38 -0700

    sum_of_primes including how to find primes and placing asserts

--- sum_of_primes.py
+++ sum_of_primes.py
@@ -1,17 +1,27 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
-def sum(num1, num2):
-    for x in range(num1, num2):
-        for y in range(20, x):
-            if y % x == 0:
-                return y
-    result = (x + y)
+def is_primes(n):
+    if n <= 1:
+        return False
+    for x in range(2, n):
+        if x ** 2 > n:
+            break
+        if n % x == 0:
+            return False
+    return True
 
+
+def sum(num1):
+    result = 0
+    for x in range(num1 + 1):
+        if is_primes(x):
+            result += x
     return result
 
-
-if __name__ == '__main__':
-    result = sum(1999999, 2000000)
-    print(result)
-    assert sum(1999999, 2000000) == 3999997
+if __name__ == "__main__":
+    assert sum(5) == 10
+    assert sum(17) == 58
+    assert sum(224) == 4661
+    assert sum(2000000) == 142913828922
+    print("test passed!")
