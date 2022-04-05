e89f1695e917d222e066345aaf9a1e503e5e532c
Nestor Vega <vega.nestor@yahoo.com>
2022-04-04 19:01:43 -0700

    romans to nums running code

--- romans_to_nums.py
+++ romans_to_nums.py
@@ -1,5 +1,4 @@
-# "MCDLVI" == 1456
-# "MCMXCIV" == 1994
+
 def romanToInt(input_string: str) -> int:
     roman = {
         "I": 1,
@@ -11,25 +10,28 @@ def romanToInt(input_string: str) -> int:
         "M": 1000
     }
     output = 0
+    last_char = None
     for char in input_string:
         value = roman[char]
-        if input_string >= "IV":
-            output = value - output
-        elif input_string == "IX":
-            output = value - output
-        elif input_string == "XL":
-            output = value - output
-        elif input_string == "XC":
-            output = value - output
-        elif input_string == "CD": 
-            output = value - output
-        elif input_string == "CM": 
-            output = value - output
-        else:
-            output += value
+        output += value
+        if last_char:
+            if last_char == "I" and char in ["V", "X"]:
+                 output -= 2 
+            if last_char == "X" and (char == "L" or char == "C"):
+                 output -= 20 
+            if last_char == "C" and (char == "D" or char == "M"):
+                 output -= 200 
+        last_char = char
+
     return output
 
 
 if __name__ == "__main__":
-    output = romanToInt("MCMXCVI")
-    print(output)
+    print("running tests for romanToInt...")
+    assert romanToInt("I") == 1
+    assert romanToInt("IV") == 4
+    assert romanToInt("VI") == 6
+    assert romanToInt("MCD") == 1400
+    assert romanToInt("MCDLVI") == 1456
+    assert romanToInt("MCMXCIV") == 1994
+    print("test passed!")
