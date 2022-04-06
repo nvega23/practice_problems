fc27f3f7e4029bfb0de3ec69ea925d62271d8461
Nestor Vega <vega.nestor@yahoo.com>
2022-04-06 09:43:54 -0700

    romans to nums solo code

--- romans_to_nums_solo.py
+++ romans_to_nums_solo.py
@@ -1,3 +1,15 @@
+""" Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII. Instead, the number four is written
+as IV. Because the one is before the five we subtract it making four. The same
+principle applies to the number nine, which is written as IX. There are six
+instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. """
+
+
 def romanToInt(input_string: str) -> int:
 	romans = {
 		"I": 1,
@@ -22,9 +34,7 @@ def romanToInt(input_string: str) -> int:
 				output -= 200 
 		last_character = char
 
-
 	return output
-
 
 
 if __name__ == "__main__":
@@ -33,4 +43,6 @@ if __name__ == "__main__":
 	assert romanToInt("IV") == 4
 	assert romanToInt("VI") == 6
 	assert romanToInt("IX") == 9
+	assert romanToInt("MCDLVI") == 1456
+	assert romanToInt("MCMXCIV") == 1994
 	print("Test passed!")
