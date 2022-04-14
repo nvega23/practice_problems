badbb7b72e44fa62cfb260d3a88e520bb1a61e51
Nestor Vega <vega.nestor@yahoo.com>
2022-04-13 19:16:20 -0700

    print statements

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -1,19 +1,25 @@
-# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 2520 is the smallest number that can be divided
+# by each of the numbers from
+# 1 to 10 without any remainder.
 
-# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# What is the smallest positive number that
+# is evenly divisible by all of the
+# numbers from 1 to 20?
 
 
 def smallest_number(num1):
-    for x in range(2, 21):
+    for x in range(1, 11):
         if num1 % x != 0:
             return False
     return True
-     
+n = 1
+while True:
+    if smallest_number(n):
+        break
+    n += 1
+
 
 if __name__ == '__main__':
-    number = 20
-    while True:
-        if smallest_number(number):
-            break
-        number += 20
-    print(f'{number:,}')
+    print("running smallest multiple")
+    print(f'{n:,}')
+    print("test passed!")
