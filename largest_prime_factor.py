76a9fa0343c7b11e5c7172862fc025afe3b2cea1
Nestor Vega <vega.nestor@yahoo.com>
2022-04-21 10:41:13 -0700

    finding the largest prime factors

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -2,26 +2,25 @@
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143? 
+if not prime keep on dividing
 """
-def is_prime(n):
-	if n <= 2:
-		return False
-	for x in range(2, num1):
-		if x ** 2 > n:
-			break
-		if n % x == 0:
-			return False
-	return True
-
-def factors(num1):
-	output = ()
-	if is_prime == False:
-		for x in range(2, num1):
-			if x % num1 == 0:
-				output += x
-		return output
+def factor(x):
+	prime_factor = []
+	div = 2
+	while div <= x:
+		if x % div == 0:
+			prime_factor.append(div)
+			x = x / div
+		else:
+			div += 1
+	return prime_factor
 
 
 if __name__ == "__main__":
-	output = factors(10)
-	print(output)
+	print("running largest prime factor...")
+	assert factor(15) == [3, 5]
+	assert factor(159) == [3, 53]
+	assert factor(12345) == [3, 5, 823]
+	assert factor(13195) == [5,7,13,29]
+	assert factor(600851475143) == [71, 839, 1471, 6857]
+	print("test passed!")
